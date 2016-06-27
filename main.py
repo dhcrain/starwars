ec087887ccef39c9245c97470a1d36c49efcfb78
dhcrain <dhcrain@gmail.com>
2016-06-27 14:08:29 -0400

    basic 3 options, people, films, vehicals

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import requests
 
-url = "http://swapi.co/api/species/1/"
+# url = "http://swapi.co/api/people/"
+# response = requests.get(url).json()
+# print(response)
 
-response = requests.get(url).json()
+# url = "http://swapi.co/api/people/"
+# response = requests.get(url).json()
+# for item in response['results']:
+#     print(item['name'])
 
-print(response)
+choice = int(input("""
+Star Wars
+1: Characters
+2: Films
+3: Vehicles
+"""))
+
+if choice == 1:
+    url = "http://swapi.co/api/people/"
+    response = requests.get(url).json()
+    for item in response['results']:
+        print(item['name'])
+
+if choice == 2:
+    url = "http://swapi.co/api/films/"
+    response = requests.get(url).json()
+    for item in response['results']:
+        print(item['title'])
+
+if choice == 3:
+    url = "http://swapi.co/api/vehicles/"
+    response = requests.get(url).json()
+    for item in response['results']:
+        print(item['name'])
