4a2bf28571347da545ff1d6a4e330bf11bd2ca9a
dhcrain <dhcrain@gmail.com>
2016-06-27 21:37:09 -0400

    working in test file from old commit to firgure out where I went wrong

--- main.py
+++ main.py
@@ -5,51 +5,71 @@ import re
 def print_results(response, dict_key):
     for item in response['results']:
         # get the number out of the URL, http://stackoverflow.com/questions/26825729/extract-number-from-string-python
-        item_url = int((re.findall("\d+", item['url'])[0]))
+        item_url = int(re.findall("\d+", item['url'])[0])
         print("{}: {}".format(item_url, item[dict_key]))
 
+def detail_more_menu():
+    ask = (input("""
+    Number for details
+    See more entries (Y)
+    Main Menu (N) """)).lower()
+    return ask
 
 def call_swapi(data):
     url, dict_key = data
     response = requests.get(url).json()
-
+    print_results(response, dict_key)
+    detail_choice = detail_more_menu()
     if response['next']:
-        print_results(response, dict_key)
+        if detail_choice == "y":
+            # while response['next']:
+            url = response['next']
+            response = requests.get(url).json()
+            print_results(response, dict_key)
+            detail_choice = detail_more_menu()
+            detail_more_menu()
+            if detail_choice == "n":
+                welcome()
+            if detail_choice == "y":
+                call_swapi(data)
+            else:
+                detail_choice = int(detail_choice)
+                url = "{}{}/".format(url, detail_choice)
+                response = requests.get(url).json()
+                print(response['name'])
 
-        detail_choice = (input("""
-        Number for details
-        See more entries (Y)
-        Main Menu (N) """))
-
-        if isinstance(detail_choice, int ):
-        # detail_choice = int(input("What item do you want to see the deatils for: "))
+        if detail_choice == "n":
+            welcome()
+        else:   # catches a number input
+            detail_choice = int(detail_choice)
             url = "{}{}/".format(url, detail_choice)
             response = requests.get(url).json()
-            print(response[dict_key])
-        # exit_contiune = input("""  """)
-
-        if detail_choice != "n":
-            while response['next']:
-                more = input("Do you want to see more? Y/n \n").lower()
-                if more != "n":
-                    url = response['next']
-                    response = requests.get(url).json()
-                    print_results(response, dict_key)
-                else:
-                    welcome()
+            print(response['name'])
     else:
         print_results(response, dict_key)
-
+        # need the detail ask func here, when I make it....
 
 def welcome():
+    # http://www.pc-freak.net/ascii-art-pictures/star-wars/starwars_title.txt
     choice = int(input("""
-Star Wars
-1: Characters
-2: Films
-3: Vehicles
-4: Exit
-------------"""))
+                 d888888888888888888  d8888b    8888888888b
+                 Y888888888888888888 d88PY88b   88888888888b
+                  Y888b    88888    ,88P  Y88.  888R    X88P
+                   Y888b   88888    d88'  `88b  8888bood88P
+           8888888888888b  88888   ,8888888888. 8888PY88888888888
+           8888888888888P  88888   888P    Y888 8888  Y8888888888
 
+           Y88b   d88b   d88P  d8888b    8888888888b  d8888888888
+           `888b d8888b d888' d88PY88b   88888888888b Y8888888888
+            Y888V888888V888P ,88P  Y88.  888R    X88P  Y888b
+            `888888PY888888' d88'  `88b  8888bood88P    Y888b
+             Y8888P  Y8888P ,8888888888. 8888PY8SSt&cgmm88888b
+              Y88P    Y88P  888P    Y888 8888  Y8888888888888P
+What information would you like to see?
+=========================================================================
+1: Characters                       3: Vehicles
+2: Films                            4: Exit
+========================================================================="""))
     if choice == 4:
         exit()
     else:
@@ -58,8 +78,6 @@ Star Wars
         2: ["http://swapi.co/api/films/", "title"],
         3: ["http://swapi.co/api/vehicles/", "name"],
         }
-
     call_swapi(choice_dict[choice])
-
 while True:
     welcome()
